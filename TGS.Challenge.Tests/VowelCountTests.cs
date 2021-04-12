82f298ba9968d52657e54762ce9a8465dcd35927
Thinus <Thinusdupisanie@gmail.com>
2021-04-12 08:39:35 +0200

    Amend AEIOU_Returns_Correct_Count() unit test as the number of characters will never be more 5 based on the input value given

--- TGS.Challenge.Tests/VowelCountTests.cs
+++ TGS.Challenge.Tests/VowelCountTests.cs
@@ -19,13 +19,16 @@ namespace TGS.Challenge.Tests
             Assert.Throws<ArgumentException>(() => _vowelCount.Count(string.Empty));
         }
 
-
+        //Original Test:Assert.AreEqual(6, count);
+        //this will always fail as the number of characters you pass in is only 5
+        //I amended the test to equal the correct amount of characters
         [Test()]
         public void AEIOU_Returns_Correct_Count()
         {
             var count = _vowelCount.Count("AEIOU");
 
-            Assert.AreEqual(6, count);
+            //Amended value from 6 to 5
+            Assert.AreEqual(5, count);
         }
 
         [Test()]
